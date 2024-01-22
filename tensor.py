7c720bffd365da10511d6ad91ab20c6662462b9c
GryphonBro <realpikapikachu@gmail.com>
2024-01-22 23:50:35 +0100

    Update tensor.py

--- tensor.py
+++ tensor.py
@@ -1,6 +1,19 @@
-__import__('libs/torch')
-__import__("urllib")
-__import__('libs/bs4')
+def importAllModules(folderPath):
+    # Ottieni l'elenco dei file nella cartella
+    files = os.listdir(folderPath)
+    
+    # Importa tutti i moduli nella cartella
+    for file_name in files:
+        module_name = file_name.split('.')[0]  # Rimuovi l'estensione .py
+        module_path = folderPath + '/' + module_name
+        
+        # Usa __import__ per importare il modulo
+        __import__(module_path)
+
+# Importa tutti i moduli nelle cartelle "bs4", "torch" e "urllib"
+importAllModules('libs/bs4')
+importAllModules('libs/torch')
+importAllModules('libs/urllib')
 
 #import libs.torch
 #import libs.urllib
